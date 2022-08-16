e870cff4060057d30acaab57584db5aa5d487e22
Jonathan Cernero <cernero.jon@gmail.com>
2022-08-16 16:10:40 -0400

    first heroku deployment

--- src/Utilities/Environments.ts
+++ src/Utilities/Environments.ts
@@ -4,6 +4,8 @@ switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3000';
     break;
+  case 'jac-accounthubclient.herokuapp.com':
+    APIURL = 'https://jac-accounthubclient.herokuapp.com';
 }
 
 export default APIURL;
